7941436dbaadc6fe7da459314984ba610d6b0024
Krishna Prasad Varma <prasad@rightsolutions.io>
2017-09-24 11:28:01 +0400

    Decorators loaded so that main app could override models

--- lib/kuppayam/engine.rb
+++ lib/kuppayam/engine.rb
@@ -22,6 +22,12 @@ module Kuppayam
       end
     end
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec, :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
