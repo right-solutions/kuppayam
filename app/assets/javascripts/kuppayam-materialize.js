03de72934e1a951798a264191f62237cc6536a05
Abdul Anas <anasabdulanas3@gmail.com>
2018-03-24 15:11:36 +0530

    commit js for tree menu

--- app/assets/javascripts/kuppayam-materialize.js
+++ app/assets/javascripts/kuppayam-materialize.js
@@ -12,6 +12,7 @@
 //
 //= require kuppayam/utilities.js
 //= require toastr/toastr.min.js
+//= require materialize/vanillatree.js
 //= require materialize/vendors.min.js
 //= require materialize/app.min.js
 //= require materialize/hoe.js
@@ -22,3 +23,143 @@
 // require uikit/js/addons/autocomplete.js
 // require tagsinput/bootstrap-tagsinput.min.js
 //= require_self
+
+
+    var treeView = document.getElementsByClassName("treeview"),
+    tree = new VanillaTree( treeView, {
+    });
+
+
+
+    tree.add({
+      label: 'Konica Minolta',
+      // id: 'a',
+      id: 'KonicaMinolta',
+      opened: true
+    });
+
+    tree.add({
+      label: 'UAE',
+      parent: 'KonicaMinolta',
+      // id: 'a.a',
+      id: 'KM-UAE',
+      opened: true,
+      selected: true
+    });
+
+        tree.add({
+          label: 'Dubai',
+          parent: 'KM-UAE',
+          // id: 'a.a',
+          id: 'KM-Dubai',
+          opened: true,
+          selected: false
+        });
+            tree.add({
+              label: 'Juma Al Majid',
+              parent: 'KM-Dubai'
+            });
+            tree.add({
+              label: 'ACS',
+              parent: 'KM-Dubai'
+            });
+
+
+            tree.add({
+              label: 'Abu Dhabi',
+              parent: 'KM-UAE',
+              // id: 'a.a',
+              id: 'KM-AbuDhabi',
+              opened: true,
+              selected: false
+            });
+                tree.add({
+                  label: 'Al Mulla Group',
+                  parent: 'KM-AbuDhabi'
+                });
+                tree.add({
+                  label: 'MHD',
+                  parent: 'KM-AbuDhabi'
+                });
+
+
+            tree.add({
+              label: 'Sharjah',
+              parent: 'KM-UAE',
+              // id: 'a.a',
+              id: 'KM-Sharjah',
+              opened: true,
+              selected: false
+            });
+                tree.add({
+                  label: 'HCL',
+                  parent: 'KM-Sharjah'
+                });
+                tree.add({
+                  label: 'A&P',
+                  parent: 'KM-Sharjah'
+                });
+
+
+
+
+
+      tree.add({
+      label: 'Saudi Arabia',
+      parent: 'KonicaMinolta',
+      // id: 'a.a',
+      id: 'KM-SaudiArabia',
+      opened: true,
+      selected: false
+    });
+
+
+        tree.add({
+          label: 'Riyadh',
+          parent: 'KM-SaudiArabia',
+          // id: 'a.a',
+          id: 'KM-Riyadh',
+          opened: true,
+          selected: false
+        });
+        
+        tree.add({
+          label: 'ABC',
+          parent: 'KM-Riyadh'
+        });
+
+
+        tree.add({
+          label: 'Jeddah',
+          parent: 'KM-SaudiArabia',
+          // id: 'a.a',
+          id: 'KM-Jeddah',
+          opened: true,
+          selected: false
+        });
+        
+        tree.add({
+          label: 'XYC',
+          parent: 'KM-Jeddah'
+        });
+       
+
+
+            
+
+
+    // // -----------------------------
+
+
+
+    treeView.addEventListener('vtree-open', function(evt) {
+      info.innerHTML = evt.detail.id + ' is opened';
+    });
+
+    treeView.addEventListener('vtree-close', function(evt) {
+      info.innerHTML = evt.detail.id + ' is closed';
+    });
+
+    treeView.addEventListener('vtree-select', function(evt) {
+      info.innerHTML = evt.detail.id + ' is selected';
+    });
